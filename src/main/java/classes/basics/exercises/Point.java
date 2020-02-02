bfb95db8146f07bedba715b5d2599ba38cf311f3
hxx86 <60172160+hxx86@users.noreply.github.com>
2020-02-02 12:30:35 +0100

    add point collection class

--- src/main/java/classes/basics/exercises/Point.java
+++ src/main/java/classes/basics/exercises/Point.java
@@ -1,4 +1,28 @@
 package classes.basics.exercises;
 
 public class Point {
+
+    double x;
+    double y;
+
+    Point(double x, double y) {
+        this.x = y;
+        this.y = x;
+
+
+
+    }
+    //metoda statyczna
+    static double distance(Point point1, Point point2) {
+        return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2));
+    }
+
+    //Przeciążenie metody to nazwa z paramatrami metody
+
+    double distanceTo (Point other){
+        return distance(this, other);
+
+    }
+
+
 }
---
+++ src/main/java/classes/basics/exercises/Test.java
@@ -0,0 +1,18 @@
+package classes.basics.exercises;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Point point1 = new Point(3.0D, 1.0D);
+        Point point2 = new Point(4.0D, 7.0D);
+
+        double distance =Point.distance(point1, point2);
+        System.out.println(distance);
+
+        distance = point1.distanceTo(point2);
+        System.out.println(distance);
+
+        distance = point2.distanceTo(point1);
+        System.out.println(distance);
+    }
+}
