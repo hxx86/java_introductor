3e3adf8aacbdaa41d696493976e27688a665e642
hxx86 <60172160+hxx86@users.noreply.github.com>
2020-02-02 10:50:00 +0100

    basisc exemplex for @ clone users tests
    Add basics package

--- src/main/java/classes/Test.java
+++ src/main/java/classes/Test.java
@@ -1,4 +1,32 @@
 package classes;
 
+import com.sun.jmx.snmp.SnmpUnknownAccContrModelException;
+
+import javax.jws.soap.SOAPBinding;
+
 public class Test {
+
+    public static void main(String[] args) {
+        User adam = new User("Adam", "kowalski", 45);
+       //  adam.name = "Adam";
+        // adam.lastname = "Kowalski";
+      //  adam.age = 36;
+       // System.out.println(adam.name);  //nazwa andam . pole name
+      //  System.out.println(adam.lastname);
+      //  System.out.println(adam.age);
+
+        User jozek = new User("jozek", "nowak", 34);
+       // jozek.name = "Jozek";
+        //jozek.lastname = "nowak";
+       // jozek.age = 42;
+        jozek.introduce();
+
+        User cloneJozek = new User(jozek);
+        cloneJozek.introduce();
+        cloneJozek.name = "Robert";
+        cloneJozek.introduce();
+
+        System.out.println("Jozek:" + jozek + "  ,Robert: " + cloneJozek);
+
+    }
 }
--- src/main/java/classes/User.java
+++ src/main/java/classes/User.java
@@ -5,6 +5,24 @@ class User {
     String lastname;
     int age;
 
+    User(String name, String lastname, int age){
+        this.name=name;
+        this.lastname=lastname;
+        this.age=age;
+    }
+
+    User(User other) {
+        name = other.name;
+        lastname = other.lastname;
+        age = other.age;
+
+    }
+
+    void introduce() {
+        //ciało metody, jesli byly by parametry dodajemy this.name this.lastname itp
+        System.out.println("imię: " + name + ", nazwisko: " + lastname + ", wiek: " + age);
+    }
+
 
 
 }
