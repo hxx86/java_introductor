5b562545da5b717739c593f237f9cf8757f2107d
hxx86 <60172160+hxx86@users.noreply.github.com>
2020-02-02 08:53:30 +0100

    homework exercise 4

--- src/main/java/arrays/Exercise2.java
+++ src/main/java/arrays/Exercise2.java
@@ -1,19 +1,21 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class Exercise2 {
 
     public static void main(String[] args) {
 
-        String [] names = new String[] {
-          "Ala",
-          "Ola",
-          "Andrzej",
-          "Brunchilda",
-          "Bożydar",
-          "Sławomir",
-          "Stanisław",
-          "władysław",
-          "Petronela",
+        String[] names = new String[]{
+                "Ala",
+                "Ola",
+                "Andrzej",
+                "Brunchilda",
+                "Bożydar",
+                "Sławomir",
+                "Stanisław",
+                "władysław",
+                "Petronela",
 
         };
         /*
@@ -23,17 +25,46 @@ public class Exercise2 {
         4 znajdz najdłużesze imię
 
          */
-        for (String name: names) {
+        for (String name : names) {
             if (name.endsWith("a")) {
-                System.out.println(name+ " to imie jest żeńskie " );
+                System.out.println(name + " to imie jest żeńskie ");
             } else {
-                System.out.println(name+ "to imię jest męskie ");
+                System.out.println(name + " to imię jest męskie ");
+
+            }
+            if (name.length() == 3 && name.endsWith("a")) {  //w string length metoda jest funkcja
+                {
+                    System.out.println(name + " to imię żeńskie 3 literowe");
+                }
+
+
+                if (name.endsWith("mir") | name.endsWith("ław")) {
+                    System.out.println(name + " to imię słowiańskie");
+                } else {
+                    System.out.println(name + " to imię nieslowianskie");
+                }
             }
 
 
-
-
         }
+
+        { StringBuilder longestName = new StringBuilder();
+        StringBuilder tempLongestName = new StringBuilder();
+        int longestLength = 0;
+        for (String name : names) {
+            if (name.length() == longestLength) {
+                tempLongestName.append(name).append(" ");
+                longestName.replace(0, longestName.length(), String.valueOf(tempLongestName));
+            }
+            if (name.length() > longestLength) {
+                longestLength = name.length();
+                tempLongestName.replace(0, tempLongestName.length(), name + " ");
+                longestName.replace(0, longestName.length(), String.valueOf(tempLongestName));
+            }
+        }
+        System.out.println("Najdłuższe imię/imiona to: " + longestName);
     }
 
+
+    }
 }
